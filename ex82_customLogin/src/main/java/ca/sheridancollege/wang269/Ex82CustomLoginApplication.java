9e780f829a33947404b24be88195f46b34548ee3
Kevin Wang <105396726+Kev-En-Wang@users.noreply.github.com>
2023-01-23 14:07:55 -0500

    Add files via upload

---
+++ ex82_customLogin/src/main/java/ca/sheridancollege/wang269/Ex82CustomLoginApplication.java
@@ -0,0 +1,13 @@
+package ca.sheridancollege.wang269;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex82CustomLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ex82CustomLoginApplication.class, args);
+	}
+
+}
